c0511befa762127c56a76c39d98344c4ab8e006d
Akira <akira.aida@uoit.net>
2017-10-11 12:54:18 -0400

    Adding Empty Android Project
    - Created an empty android project
      - Project was called workplace_app
      - company domain was csci4100
    - Moved the project.md into the new doc/ dir

---
+++ src/workplace_app/app/src/main/java/ca/uoit/csci4100u/workplace_app/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.uoit.csci4100u.workplace_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
