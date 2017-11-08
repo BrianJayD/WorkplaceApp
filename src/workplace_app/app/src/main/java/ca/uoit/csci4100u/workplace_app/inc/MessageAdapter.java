067c067b8433f4b5e982fcf0c52a95f6695f7d1e
Akira <akira.aida@uoit.net>
2017-11-08 16:07:15 -0500

    Changed MessageAdapter to Work Better
    - Added comments to the MessageAdapter class
    - Made updates to make the adapter cleaner
    - Styled the list view and list items to a degree
    - Made the color of the person logged in magenta when they speak

--- src/workplace_app/app/src/main/java/ca/uoit/csci4100u/workplace_app/inc/MessageAdapter.java
+++ src/workplace_app/app/src/main/java/ca/uoit/csci4100u/workplace_app/inc/MessageAdapter.java
@@ -1,41 +1,65 @@
 package ca.uoit.csci4100u.workplace_app.inc;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.List;
+import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.List;
 import ca.uoit.csci4100u.workplace_app.R;
 
+/**
+ * A customer adapter for the messages' list view in the 'ChatActivity'
+ */
 public class MessageAdapter extends ArrayAdapter<Message> {
-    private Context context;
-    private List<Message> data;
 
-    public MessageAdapter(Context context, List<Message> data) {
-        super(context, R.layout.message_list_item, data);
-        this.data = data;
-        this.context = context;
+    private Context mContext;
+    private List<Message> mMessages;
+
+    /**
+     * Constructor for the MessageAdapter
+     * @param context The context of the activity to be using this adapter
+     * @param messages A list of messages each storing message data
+     */
+    public MessageAdapter(Context context, List<Message> messages) {
+        super(context, R.layout.message_list_item, messages);
+        this.mMessages = messages;
+        this.mContext = context;
     }
 
+    /**
+     * A helper function to determine the number of messages
+     * @return The number of messages
+     */
     public int getCount() {
-        return data.size();
+        return mMessages.size();
     }
 
-    public long getItemId(int position) {
-        return position;
-    }
-
+    /**
+     * A function to display a custom layout for each message list item
+     * @param position The position of the list item in the list
+     * @param convertView The view the current list is displaying
+     * @param parent The parent view (the list view)
+     * @return The list view items to be returned
+     */
     public View getView(int position, View convertView, ViewGroup parent) {
-        Message messageToDisplay = data.get(position);
+        Message messageToDisplay = mMessages.get(position);
 
         if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(context);
+            LayoutInflater inflater = LayoutInflater.from(mContext);
             convertView = inflater.inflate(R.layout.message_list_item, parent, false);
         }
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (messageToDisplay.getUserId().compareTo(auth.getCurrentUser().getUid()) == 0) {
+            View viewById = convertView.findViewById(R.id.messageItem);
+            viewById.setBackgroundColor(Color.MAGENTA);
+        }
+
         TextView lblName = convertView.findViewById(R.id.lblUserName);
         lblName.setText(messageToDisplay.getUserName());
 
