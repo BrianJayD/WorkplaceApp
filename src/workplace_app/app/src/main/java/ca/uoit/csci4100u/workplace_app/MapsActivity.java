c3319deeb5a21c28c13fe75d60a9da8452280d83
Saif Niaz <saifniaz.3@gmail.com>
2017-11-30 18:22:08 -0500

    Updated the mapActivity

--- src/workplace_app/app/src/main/java/ca/uoit/csci4100u/workplace_app/MapsActivity.java
+++ src/workplace_app/app/src/main/java/ca/uoit/csci4100u/workplace_app/MapsActivity.java
@@ -1,5 +1,12 @@
 package ca.uoit.csci4100u.workplace_app;
 
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -10,9 +17,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        if(manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> list = geocoder.getFromLocation(latitude, longitude, 1);
+                        String data = list.get(0).getAddressLine(0);
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(data));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.2f));
+                        mMap.setTrafficEnabled(true);
+                        mMap.setBuildingsEnabled(true);
+                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }else if(manager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    double latitude = location.getLatitude();
+                    double longitude = location.getLongitude();
+
+                    LatLng latLng = new LatLng(latitude, longitude);
+
+                    Geocoder geocoder = new Geocoder(getApplicationContext());
+                    try {
+                        List<Address> list = geocoder.getFromLocation(latitude, longitude, 1);
+                        String data = list.get(0).getAddressLine(0);
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(data));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.2f));
+                        mMap.setTrafficEnabled(true);
+                        mMap.setBuildingsEnabled(true);
+                        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+        }
+
+
     }
 
 
@@ -39,8 +142,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 10.2f));
     }
 }
